7ca7e31fe062dbf93f573818b1ef7b24e191895a
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-10-21 22:01:50 -0600

    Started working on my server.js file and added some code from an activity we did class to get started with

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+/*
+function displayHtml(res){
+   const code
+  res.set('Content-type', 'text/html');
+  res.status(404).send(code);
+  
+}
+*/
+
+// Routes 
+// ======================================
+
+// app.get("/", function(req, res){
+//   return displayHtml(res);
+// });
+
+// Listener
+// =====================================
+
+// app.listen(PORT, function(){
+//   console.log("App listening of PORT " + PORT);
+// });
