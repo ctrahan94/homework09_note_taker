04d963974cf1fc108f0aa66fa0f1565c648f2fa5
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-10-24 22:39:24 -0600

    Finally was able to get the delete button to work

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -18,7 +18,7 @@ router.post("/notes", function (req, res) {
 
 router.delete("/notes/:id", function (req, res) {
   store
-    .removeNote(req.params.id)
+    .deleteNote(req.params.id)
     .then(res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
 });
