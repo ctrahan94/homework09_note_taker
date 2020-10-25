4297a2e6d16333a8b456a080c6d8f0906ae54f1d
Colby Trahan <ctrahan94@users.noreply.github.com>
2020-10-24 21:55:12 -0600

    I have everything working, but the delete button

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const path = require("path");
 const express = require("express");
+const path = require("path")
+const fs = require("fs")
+const apiRoutes = require("./routes/apiroutes")
+const htmlRoutes = require("./routes/htmlroutes")
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", function( req, res){
-  res.json(path.join(__dirname, "public/index.html"));
-});
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 
-// Listener
-// =====================================
-
-app.listen(PORT, function(){
-  console.log("App listening of PORT " + PORT);
-});
-
-
-
-
-
-//so in theory, you could do res.send(req.body.name) as a variable and then use that variable in an fs.writefile to send that info to lets say…a db.json
+app.listen(PORT, function () {
+    console.log(`App listening on PORT: ${PORT}`)
+})
